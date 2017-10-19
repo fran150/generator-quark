33ffdb4a03445ea24db391954a247679c43dbdfb
admin <fllopez@bpba.com.ar>
2017-10-19 17:58:01 -0300

    Added service generator for modules

--- service/index.js
+++ service/index.js
@@ -6,7 +6,7 @@ var chalk = require('chalk');
 var slugify = require('slugify');
 var pascalCase = require('pascal-case');
 
-var QuarkScreenGenerator = class extends Generator {
+var QuarkServiceGenerator = class extends Generator {
 
     initializing() {
         this.log(chalk.blue("   ____                   _      _            "));
@@ -18,7 +18,7 @@ var QuarkScreenGenerator = class extends Generator {
         this.log(chalk.blue("                               _/ |           "));
         this.log(chalk.blue("                               |__/           "));
         this.log(chalk.magenta('You\'re using the fantastic Quark SCREEN generator!!!'));
-
+        
         this.argument('path', {
             required: true,
             type: String,
@@ -37,11 +37,10 @@ var QuarkScreenGenerator = class extends Generator {
         this.info.name = this.info.paths[this.info.paths.length - 1];
 
         // Define base dir for require and file
-        this.info.servicesReqBase = 'services';
         this.info.servicesFileBase = 'src/services';
 
         // Get the model and view file name
-        this.info.serviceFileName = this.info.path + '.service.js';
+        this.info.serviceFileName = this.info.servicesFileBase + '/' + this.info.path + '.service.js';
 
         var moduleMain = this.destinationPath('src/main.js');
         var moduleConfig = this.destinationPath('src/main.json');
@@ -52,29 +51,11 @@ var QuarkScreenGenerator = class extends Generator {
         var bowerContent = this.fs.read(bowerPath);
         var bower = JSON.parse(bowerContent);
 
-
-        // Get the require path for model and view
-        this.info.modelReqPath = this.info.componentsReqBase + folder + '/' + this.info.modelName;
-        this.info.viewReqPath = this.info.componentsReqBase + folder + '/' + this.info.viewName;
-
+        this.info.bower = bower;
+        
         // Get the class name from tag
-        this.info.className = pascalCase(this.options['tag']) + 'Screen';
-    }
-
-    prompting() {
-        // If the noTest flag is NOT applied prompt for tests
-        if (!this.info.noTest) {
-            return this.prompt([{
-                type    : 'confirm',
-                name    : 'tests',
-                message : 'Do you want to generate test files for this screen?',
-                default : true
-            }]).then(function (answers) {
-                this.info.tests = answers.tests;
-            }.bind(this));
-        } else {
-            this.info.tests = false;
-        }
+        this.info.serviceName = pascalCase(this.info.path);
+        this.info.className = this.info.serviceName + "Service";
     }
 
     _addNamespace(config, namespaces) {
@@ -129,101 +110,43 @@ var QuarkScreenGenerator = class extends Generator {
 
     writing() {
 
-        this.log('Generating screen...');
+        this.log('Generating service...');
         this.fs.copyTpl(
-            this.templatePath('model.js'),
-            this.destinationPath(this.info.modelPath),
-            this.info
-        );
-        this.fs.copyTpl(
-            this.templatePath('view.html'),
-            this.destinationPath(this.info.viewPath),
+            this.templatePath('service.js'),
+            this.destinationPath(this.info.serviceFileName),
             this.info
         );
 
-        this.log('Registering screen...');
-        // Get the components JSON
-        var jsonPath = this.destinationPath('src/app/config/components/screens.config.json');
-        var content = this.fs.read(jsonPath);
-        var config = JSON.parse(content);
+        this.log('Registering service...');
+        
+        if (this.info.isModule) {
+            // Get the components JSON
+            var jsonPath = this.destinationPath('src/main.json');
+            var content = this.fs.read(jsonPath);
+            var json = JSON.parse(content);
 
-        // Clone the namespaces array
-        var namespaces = this.info.namespaces.slice();
-        config = this._addNamespace(config, namespaces);
-
-        // Sort the objects keys
-        var ordered = this._sortObjectKeys(config);
-
-        // Rewrite JSON file
-        this.fs.write(jsonPath, JSON.stringify(ordered, null, 4));
-
-        // If Tests are be enabled
-        if (this.info.tests) {
-            this.log('Generating tests...');
-
-            // Generate test spec
-            this.fs.copyTpl(
-                this.templatePath('tests/specs/screen.test.js'),
-                this.destinationPath('tests/specs/' + this.info.tag + '.test.js'),
-                this.info
-            );
-            // Generate view
-            this.fs.copyTpl(
-                this.templatePath('tests/views/screen.html'),
-                this.destinationPath('tests/views/' + this.info.tag + '.html'),
-                this.info
-            );
-
-
-            this.log('Registering test...');
-
-            // Read the specs config
-            jsonPath = this.destinationPath('tests/app/config/specs.config.json');
-            content = this.fs.read(jsonPath);
-            config = JSON.parse(content);
-
-            // If the spec name is not used
-            var specName = this.info.tag + '.test';
-            if (!config.includes(specName)) {
-                // Add the spec name, sort the array a
-                config.push(specName);
-
-                config.sort();
-
-                this.fs.write(jsonPath, JSON.stringify(config, null, 4));
-            }
-        }
-
-        // ifnthe nobuild option is NOT specified
-        if (!this.info.noBuild) {
-            // Read the build config
-            this.log('Adding to build output...');
-            jsonPath = this.destinationPath('gulp.conf.json');
-            content = this.fs.read(jsonPath);//
-            config = JSON.parse(content);
-
-            var exists = config.include.includes(this.info.modelReqPath);
-
-            if (!exists) {
-                for (var name in config.bundles) {
-                    var bundle = config.bundles[name];
-
-                    if (bundle.includes(this.info.modelReqPath)) {
-                        exists = true;
-                        break;
-                    }
-                }
+            if (!json['require']) {
+                json.require = {};
             }
 
-            if (!exists) {
-                config.include.push(this.info.modelReqPath);
-
-                config.include = config.include.sort();
-
-                this.fs.write(jsonPath, JSON.stringify(config, null, 4));
+            if (!json.require['config']) {
+                json.require.config = {};
             }
+
+            if (!json.require.config['services']) {
+                json.require.config.services = {}
+            }
+
+            json.require.config.services[this.info.serviceName] = this.info.bower.name + "/" + this.info.path;
+
+            // Sort the objects keys
+            var ordered = this._sortObjectKeys(json);
+
+            // Rewrite JSON file
+            this.fs.write(jsonPath, JSON.stringify(ordered, null, 4));        
+        } else {
         }
     }
 }
 
-module.exports = QuarkScreenGenerator;
+module.exports = QuarkServiceGenerator;
---
+++ service/templates/service.js
@@ -0,0 +1,11 @@
+define([
+    'quark',
+    'knockout'
+], function ($$, ko) {
+
+    function <%- className %>() {
+        var self = this;        
+    }
+
+    return <%- className %>;
+});
